c1c487cdb8eda27f4f0fd5a80f27718f5b278cb7
Miguel Menjivar <menjivarmiguel221@gmail.com>
2019-11-14 20:24:30 -0800

    updating the files for testing purposes

---
+++ build/precache-manifest.b9441aded73478837e4d7f49a55b5b32.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "021cddf7cb406e5f6d085033226fed53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "956fe92c0a27150f60e4",
+    "url": "/static/css/2.cd45e5a2.chunk.css"
+  },
+  {
+    "revision": "cb45e45b8327e808aa39",
+    "url": "/static/css/main.44b6433c.chunk.css"
+  },
+  {
+    "revision": "956fe92c0a27150f60e4",
+    "url": "/static/js/2.78b8ab1b.chunk.js"
+  },
+  {
+    "revision": "cb45e45b8327e808aa39",
+    "url": "/static/js/main.8a6a2656.chunk.js"
+  },
+  {
+    "revision": "00372c088c9920fc1688",
+    "url": "/static/js/runtime-main.3930960f.js"
+  },
+  {
+    "revision": "9a133ed84d0c260527e511fc3aa69f16",
+    "url": "/static/media/GOT_Cover.9a133ed8.jpg"
+  },
+  {
+    "revision": "1973afa04531660821dc9a4dac69e3a1",
+    "url": "/static/media/Parks_And_Rec_Cover.1973afa0.png"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "e702ab28f2bd970c8db993591620992a",
+    "url": "/static/media/The_Office_Cover.e702ab28.png"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "2d88c542d7a54524e28b4a196afe1db3",
+    "url": "/static/media/pentatech-logo.2d88c542.png"
+  }
+]);
